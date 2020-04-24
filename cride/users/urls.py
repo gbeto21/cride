02d069c921bb98196fb923ced2f17268ea0dc1f6
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-24 13:40:53 -0500

    The users view and the serializers were created.

---
+++ cride/users/urls.py
@@ -0,0 +1,10 @@
+
+#Django
+from django.urls import path
+
+#views
+from cride.circles.views import UserLoginAPIView
+
+urlpatterns = [
+    path('users/login/', UserLoginAPIView.as_view(), name='login'),
+]
