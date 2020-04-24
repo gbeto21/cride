1e756b8dca32b202800c5f837c3e9dd5defdf1bf
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-24 10:50:46 -0500

    An new view made with rest_framework was creted.

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,16 +1,18 @@
-# Django
-from django.http import JsonResponse, HttpResponse
+# Django REST Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Models
 from cride.circles.models import Circle
 
 
+@api_view(['GET'])
 def list_circles(request):
-    circles = Circle.objects.all()
-    public = circles.filter(is_public=True)
+    # import ipdb; ipdb.set_trace()
+    circles = Circle.objects.filter(is_public=True)
     data = []
 
-    for circle in public:
+    for circle in circles:
         data.append({
             'name': circle.name,
             'slug_name': circle.slug_name,
@@ -20,4 +22,21 @@ def list_circles(request):
 
         })
 
-    return JsonResponse(data, safe=False)
+    return Response(data)
+
+
+@api_view(['POST'])
+def create_circle(request):
+    name = request.data['name']
+    slug_name = request.data['slug_name']
+    about = request.data.get('about', '')
+    circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+    data = {
+        'name': circle.name,
+        'slug_name': circle.slug_name,
+        'rides_taken': circle.rides_taken,
+        'rides_offered': circle.rides_offered,
+        'members_limit': circle.members_limit,
+
+    }
+    return Response(data)
